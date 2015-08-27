a68068a21009528ea18b30558bd68ed29e0cf2a2
marbaclo <marbaclo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-08-27 21:43:54 +0000

    gtest
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mixcluster@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MixCluster/R/summary.R
+++ pkg/MixCluster/R/summary.R
@@ -1,86 +1,71 @@
 setMethod(
   f="summary",
-  signature=c("MixClusResults"),
+  signature=c("MixClusresults"),
   function(object, ...){
       cat("**************************************************************\n\n")
       cat("DATA SET description:","\n")
-      cat("* Number of individuals: ", object@data@n,"\n")
-      cat("* Number of variables (continuous, integer, ordinal): ", paste(sum(object@data@kind==1), 
-                                                                          sum(object@data@kind==2),
-                                                                          sum(object@data@kind==3),
-                                                                          sep=", "),"\n\n")
+      cat("* Number of individuals: ",object@data$nrow,"\n")
+      cat("* Number of variables (continuous, integer, binary, ordinal): ",paste(sum(object@data$kind==1),sum(object@data$kind==2),sum(object@data$kind==3),sum(object@data$kind==4),sep=", "),"\n\n")
       cat("**************************************************************\n\n")
       cat("MODEL description:","\n")
-      cat("* Number of classes:   ", length(object@param@pi),"\n")
-      cat("* Model:            ", class(object@model$model),"\n")
-      cat("* Log-likelihood:  ", object@model$model@loglike,"\n")
-      cat("* BIC:             ", object@model$model@bic,"\n")
-      cat("* ICL:             ", object@model$model@icl,"\n")
+      cat("* Number of classes:   ", length(object@param@proportions),"\n")
+      cat("* Model:            ", object@model,"\n")
+      cat("* Log-likelihood:  ", object@criteria$loglike,"\n")
+      cat("* BIC:             ", object@criteria$bic,"\n")
+      cat("* ICL:             ", object@criteria$icl,"\n")
       
       cat("**************************************************************\n\n")
       cat("PARAMETERS description:","\n")
-      for (k in 1:length(object@param@pi)){
+      for (k in 1:length(object@param@proportions)){
         cat("* Class", k,"\n")         
-        cat("* Proportion            ", object@param@pi[k],"\n")
-        if (any(object@data@kind==1)){
+        cat("* Proportion            ", object@param@proportions[k],"\n")
+        if (any(object@data$kind==1)){
           cat("* Margins parameters of the continuous variables (Gaussian distribution):\n")
-          tmp <- matrix(0, sum(object@data@kind==1), 2)
-          rownames(tmp)=colnames(object@data@x)[which(object@data@kind==1)]
+          tmp <- object@param@margins[[k]][which(object@data$kind==1),1:2]
+          if (sum(object@data$kind==1)==1){
+            tmp <- matrix(object@param@margins[[k]][which(object@data$kind==1),1:2],1,2)
+            rownames(tmp)=colnames(object@data$data)[which(object@data$kind==1)]
+          }
           colnames(tmp) <- c("Mean","Sd")
-          cp <- 1
-          for (j in which(object@data@kind==1)){
-            tmp[cp, ] <- c(object@param@beta[[k]][[j]]@mu, object@param@beta[[k]][[j]]@sd)  
-            cp <- cp+1
-          }
-
           print(tmp, digits=2)
           cat("\n")
         }
-        if (any(object@data@kind==2)){
+        if (any(object@data$kind==2)){
           cat("* Margins parameters of the integer variables (Poisson distribution):\n")
-   
-          
-          tmp <- matrix(0, sum(object@data@kind==2), 1)
-          rownames(tmp)=colnames(object@data@x)[which(object@data@kind==2)]
+          tmp <- as.matrix(object@param@margins[[k]][which(object@data$kind==2),1])
           colnames(tmp) <- c("Mean")
-          cp <- 1
-          for (j in which(object@data@kind==2)){
-            tmp[cp, ] <- c(object@param@beta[[k]][[j]]@beta)  
-            cp <- cp+1
-          }
+          rownames(tmp)=rownames(object@param@correlations[[k]])[which(object@data$kind==2)]
           print(tmp, digits=2)
           cat("\n")
         }
-        if (any(object@data@kind==3)){
+        if (any(object@data$kind==3)){
+          cat("* Margins parameters of the binary variables (Beta distribution):\n")
+          tmp <- object@param@margins[[k]][which(object@data$kind==3),1]
+          tmp <- cbind(tmp, 1-tmp)
+          rownames(tmp) <- rownames(object@param@correlations[[k]])[which(object@data$kind==3)]
+          colnames(tmp) <- c("Prob.0","Prob.1")
+          print(tmp, digits=2)
+          cat("\n")
+        }
+        if (any(object@data$kind==4)){
           cat("* Margins parameters of the ordinal variables (multinomial distribution):\n")
-          lg <- 0
-          for (j in which(object@data@kind==3))
-            lg <- max(lg, max(object@data@x[object@data@o[[j]],j])+1)
-          
-          tmp <- matrix(0, sum(object@data@kind==3), lg)
-          rownames(tmp) <- colnames(object@data@x)[which(object@data@kind==2)]
-          colnames(tmp) <- paste("Prob.",0:(lg-1),sep="")
-           cp <- 1
-          for (j in which(object@data@kind==3)){
-            tmp[cp, 1:(max(object@data@x[object@data@o[[j]],j])+1)] <- object@param@beta[[k]][[j]]@beta
-            cp <- cp + 1
-          }
+          tmp <- object@param@margins[[k]][which(object@data$kind==1),1:ncol(object@param@margins[[k]])]
           print(tmp, digits=2)
           cat("\n")
         }
         cat("\n")
-        if (class(object@model$model)=="MixClusModel_hetero"){
-          cat("* Correlation matrix:\n")
-          print(object@param@correl[[k]],digits=2)
+        if (object@model=="free"){
+          cat("* Correlations matrix:\n")
+          print(object@param@correlations[[k]],digits=2)
         }
         cat("\n")
         
         cat("*****************************\n")
           
       }
-      if (class(object@model$model)!="MixClusModel_hetero"){
+      if (object@model!="free"){
         cat("* Correlations matrix:\n")
-        print(object@param@correl[[k]],digits=2)
+        print(object@param@correlations[[k]],digits=2)
         
       }
 
